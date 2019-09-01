ea9d2076cb71324b8d6e7a0dd34376391d8e0a01
subarna mukhiyabeen <smukhiyabe@gannett.com>
2019-09-01 14:42:39 -0400

    add index page with navbar

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const express = require('express');
 require('dotenv').config();
 
-const port = 3000;
-const app = express();
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-fkjkr.mongodb.net/blog_app?retryWrites=true&w=majority`;
+const bodyParser = require('body-parser'),
+      mongoose = require('mongoose'),
+      express = require('express'),
+      port = 3000,
+      app = express(),
+      uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-fkjkr.mongodb.net/blog-app?retryWrites=true&w=majority`;
 
+
+
+// App config
 mongoose.connect(uri, { useNewUrlParser: true });
-
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Mongoose/Model config
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { 
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
 
 
+// Restful routes
+app.get('/', (req, res) => {
+    res.redirect('blogs');
+});
 
+app.get('/blogs', (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', {blogs: blogs});
+        }
+    });
+});
 
 app.listen(port, () => {
-    console.log(`App running on port: ${port}`);
+    console.log(`App started on port: ${port}`);
 });
